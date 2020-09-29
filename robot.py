c439f3cd52c3a45ccfd70649eeac9c0a9d515417
RC <ratherconfusing@gmail.com>
2020-09-29 23:36:15 +0200

    Added age

--- robot.py
+++ robot.py
@@ -17,6 +17,7 @@ class Robot():
         self.logger.setLevel(logging.DEBUG)
         self.battery_level = 100
         self.state = 0
+        self.age = 0
 
     def run(self, time_step=1, charge_chance_threshold=0.5):
         """ Runs the robot until battery is depleted
@@ -27,6 +28,8 @@ class Robot():
         charge_chance_threshold -- Threshold for charge <0, 1>, if chance is > this parameter, robot will charge
         """
 
+        self.logger.info(f'Simulation has started! Robot {self.name} - battery level {self.battery_level}, age: {self.age}')
+
         while True:
             time.sleep(time_step)
 
@@ -35,11 +38,11 @@ class Robot():
             if self.battery_level < 0:
                 self.battery_level = 0
 
-            self.logger.info(f'Time has progressed... Robot {self.name} has battery level {self.battery_level}..')
+            self.logger.info(f'Time has progressed... Robot {self.name} - battery level {self.battery_level}, age: {self.age}')
 
             # Check battery depletion
             if self.battery_level == 0:
-                self.logger.error(f'Robot {self.name} has ran out of juice! Game over!')
+                self.logger.error(f'Robot {self.name} has ran out of juice! Game over! Total lifespan: {self.age}')
                 break
 
             # Try to survive. If charge attempt is > charge_chance_threshold, charge by 10
@@ -48,9 +51,12 @@ class Robot():
                 self.battery_level += 10
                 if self.battery_level > 100:
                     self.battery_level = 100
-                self.logger.info(f'Robot had luck! ({charge_chance} > {charge_chance_threshold}), chargeg battery by 10, new level: {self.battery_level}')
+                self.logger.info(f'Robot had luck! ({charge_chance} > {charge_chance_threshold}), luck charged battery by 10, new level: {self.battery_level}')
             else:
                 self.logger.info('Robot did not have luck, no charge this time..')
+
+            # Robot ages as he lives inside simulation
+            self.age += 1
 
 
 if __name__=='__main__':
