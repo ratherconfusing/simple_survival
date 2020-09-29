426da2e4ecf79ee5aedff690ce79c9c36f0f6aa9
RC <ratherconfusing@gmail.com>
2020-09-29 23:30:27 +0200

    Added random charging event for survival

--- robot.py
+++ robot.py
@@ -2,7 +2,9 @@
 """File: robot.py"""
 
 import time
+import random
 import logging
+
 
 class Robot():
     def __init__(self, name='Karel'):
@@ -16,7 +18,15 @@ class Robot():
         self.battery_level = 100
         self.state = 0
 
-    def run(self, time_step=1):
+    def run(self, time_step=1, charge_chance_threshold=0.5):
+        """ Runs the robot until battery is depleted
+        Robot has a chance to charge the battery each step
+        
+        Arguments:
+        time_step -- time step of the simulation
+        charge_chance_threshold -- Threshold for charge <0, 1>, if chance is > this parameter, robot will charge
+        """
+
         while True:
             time.sleep(time_step)
 
@@ -32,6 +42,17 @@ class Robot():
                 self.logger.error(f'Robot {self.name} has ran out of juice! Game over!')
                 break
 
+            # Try to survive. If charge attempt is > charge_chance_threshold, charge by 10
+            charge_chance = random.random()
+            if charge_chance > charge_chance_threshold:
+                self.battery_level += 10
+                if self.battery_level > 100:
+                    self.battery_level = 100
+                self.logger.info(f'Robot had luck! ({charge_chance} > {charge_chance_threshold}), chargeg battery by 10, new level: {self.battery_level}')
+            else:
+                self.logger.info('Robot did not have luck, no charge this time..')
+
+
 if __name__=='__main__':
     robot = Robot()
-    robot.run()
+    robot.run(time_step=0.01)
